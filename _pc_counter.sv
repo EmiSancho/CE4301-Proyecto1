7e13f4bc7d76c737139a0cbb09857af2aa9400c3
EmiSancho <emilysancho@gmail.com>
2020-11-17 22:33:53 -0600

    final push

--- _pc_counter.sv
+++ _pc_counter.sv
@@ -7,9 +7,9 @@ module _pc_counter(clk, rst,s,se,slt,sle,sgt,sge,sle2,sle3,sle4,sle5,sle6,sle7,s
 		if (rst) actual = 0;
 		else if (s) actual = 8'b10010110; //150
 		else if (slt) actual = 8'b10010100; //148
-		else if (sle) actual = 8'b00001000 ; //8
+		else if (sle) actual = 8 ; //16
 		else if (sge) actual =  8'b10010000;//144
-		else if (sle2) actual = 8'b00011000;//24
+		else if (sle2) actual = 48;//24
 		else if (sle3) actual = 8'b00110000;//48
 		else if (sle4) actual = 8'b01001100;//76
 		else if (sle5) actual = 8'b01100010;//98
--- _pc_counter_4.sv
+++ _pc_counter_4.sv
@@ -2,6 +2,6 @@ module _pc_counter_4(pc, pcnew);
 	input logic [7:0] pc;
 	output logic [7:0] pcnew;
 
-	assign pcnew = pc + 8'd2 ;
+	assign pcnew = pc + 8'd4 ;
 
 endmodule 
